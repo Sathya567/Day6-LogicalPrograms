232bcce2b45b1e4c6652c38941bff2acea142d70
BootCampBL <satyabhandari43@gmail.com>
2021-07-12 20:23:13 +0530

    [sathya] Added - Prime Number program

---
+++ UC3_PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6Assignment
+{
+    class UC3_PrimeNumber
+    {
+        public int n;
+        public int i;
+        public int Remainder;
+        public void CheckPrimeNumber()
+        {
+            Console.WriteLine("Enter a Number"); //Input Number
+            n = int.Parse(Console.ReadLine());//Read Numbeer
+            int a = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                Console.WriteLine("is a Prime Number", n);
+            }
+            else
+            {
+                Console.WriteLine("Not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
